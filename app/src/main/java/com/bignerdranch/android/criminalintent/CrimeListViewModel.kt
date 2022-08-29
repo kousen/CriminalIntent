71379aeed39da68370a3b36ca28fdb6ec1b9e90e
Ken Kousen <ken.kousen@kousenit.com>
2022-08-29 11:53:31 -0400

    Add Room implementation

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -3,14 +3,12 @@ package com.bignerdranch.android.criminalintent
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.time.LocalDateTime
-import java.util.*
 
 private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
 
     val crimes = mutableListOf<Crime>()
 
@@ -24,17 +22,6 @@ class CrimeListViewModel : ViewModel() {
     }
 
     suspend fun loadCrimes(): List<Crime> {
-        val result = mutableListOf<Crime>()
-        delay(5000)
-        for (i in 0 until 100) {
-            val crime = Crime(
-                UUID.randomUUID(),
-                "Crime #$i",
-                LocalDateTime.now(),
-                i % 2 == 0
-            )
-            result += crime
-        }
-        return result
+        return crimeRepository.getCrimes()
     }
 }
