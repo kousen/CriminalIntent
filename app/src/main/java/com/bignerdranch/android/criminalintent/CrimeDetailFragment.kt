c9bb5a9c2ddb1430f1cb17fc2edf107d557910bc
Ken Kousen <ken.kousen@kousenit.com>
2022-08-31 13:33:37 -0400

    Clean up imports

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -1,9 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
